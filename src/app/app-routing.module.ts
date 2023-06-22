2cb42d1ffce7f1c8bcb7f567bd230a925e0dee41
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2023-06-22 17:07:28 -0500

    se añaden cambios requeridos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LocionesComponent } from './lociones/lociones.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { FooterComponent } from './footer/footer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', redirectTo: 'home', pathMatch: 'full' }, 
+            {path: 'inicio', component: HomeComponent}, {path: 'lociones', component: LocionesComponent}, {path: 'clientes', component: ClientesComponent}, {path: 'footer', component: FooterComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
